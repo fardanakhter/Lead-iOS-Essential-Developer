61d24e9ff18bae964e81c6fe09d33d445473ee6e
Fardan Akhter <fardan_akhter@hotmail.com>
2023-03-19 09:33:18 +0500

    Create FeedItem model

---
+++ 3. EssentialFeedProject/EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Fardan Akhter on 19/03/2023.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}

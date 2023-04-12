676eea3ec81e9da46fd98e7d3bdbac7d5e5c0681
Fardan Akhter <fardan_akhter@hotmail.com>
2023-04-13 00:52:07 +0500

    Move 'FeedStore' implementation of 'CodableFeedStore' to its own file

---
+++ 3. EssentialFeedProject/EssentialFeedProject/EssentialFeed/EssentialFeed/Feed Cache/Infratructure/CoreData/CoreDataFeedStore+FeedStore.swift
@@ -0,0 +1,51 @@
+//
+//  CoreDataFeedStore+FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Fardan Akhter on 13/04/2023.
+//
+
+import Foundation
+
+extension CoreDataFeedStore: FeedStore {
+    public func loadFeedCache(completion: @escaping LoadCompletion) {
+        perform { context in
+            do {
+                guard let cache = try ManagedFeedCache.find(in: context) else {
+                    return completion(.empty)
+                }
+                completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+            }
+            catch(let error) {
+                completion(.failure(error))
+            }
+        }
+    }
+   
+    public func insertFeedCache(with feed: [LocalFeedImage], and timestamp: Date, completion: @escaping InsertCompletion) {
+        perform { context in
+            do {
+                let newInstance = try ManagedFeedCache.newUniqueInstance(in: context)
+                newInstance.timestamp = timestamp
+                newInstance.feed = ManagedFeedImage.images(from: feed, in: context)
+                try context.save()
+                completion(nil)
+            }
+            catch(let error) {
+                completion(error)
+            }
+        }
+    }
+    
+    public func deleteFeedCache(completion: @escaping DeleteCompletion) {
+        perform { context in
+            do {
+                try ManagedFeedCache.find(in: context).map(context.delete).map(context.save)
+                completion (nil)
+            }
+            catch(let error) {
+                completion(error)
+            }
+        }
+    }
+}

c129af99566cf5284ce17b85e9ec124d35a6e533
Fardan Akhter <fardan_akhter@hotmail.com>
2023-04-18 08:16:59 +0500

    Update 'FeedImageCell' with prototype data to show on UI

--- 3. EssentialFeedProject/EssentialFeedProject/Prototype/Prototype/FeedViewController.swift
+++ 3. EssentialFeedProject/EssentialFeedProject/Prototype/Prototype/FeedViewController.swift
@@ -9,13 +9,25 @@ import Foundation
 import UIKit
 
 class FeedViewController: UITableViewController {
+    let feed = FeedImageViewModel.prototypeFeed
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        return feed.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "FeedCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedImageCell", for: indexPath) as! FeedImageCell
+        let model = feed[indexPath.row]
+        cell.configure(with: model)
+        return cell
     }
-    
+}
+
+extension FeedImageCell {
+    func configure(with viewModel: FeedImageViewModel) {
+        self.locationContainer.isHidden = viewModel.location == nil
+        self.locationLabel.text = viewModel.location
+        self.descriptionLabel.text = viewModel.description
+        self.feedImageView.image = UIImage(named: viewModel.image)
+    }
 }

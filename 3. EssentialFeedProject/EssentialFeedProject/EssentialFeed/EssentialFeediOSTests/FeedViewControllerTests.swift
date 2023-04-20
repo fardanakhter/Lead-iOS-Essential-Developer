ef0058aa20956941d9150b9acdccabd22e1afd94
Fardan Akhter <fardan_akhter@hotmail.com>
2023-04-20 11:08:24 +0500

    Extract test code into DSL method within extension

--- 3. EssentialFeedProject/EssentialFeedProject/EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ 3. EssentialFeedProject/EssentialFeedProject/EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -50,13 +50,11 @@ final class FeedViewControllerTest: XCTestCase {
         let (sut, loader) = makeSUT()
         
         sut.loadViewIfNeeded()
-        sut.refreshControl?.allTargets.forEach({ target in
-            sut.refreshControl?.actions(forTarget: target, forControlEvent: .valueChanged)?.forEach({
-                (target as NSObject).perform(Selector($0))
-            })
-        })
-        
+        sut.refreshControl?.simulatePullToRefresh()
         XCTAssertEqual(loader.loadCallCount, 2)
+        
+        sut.refreshControl?.simulatePullToRefresh()
+        XCTAssertEqual(loader.loadCallCount, 3)
     }
 
     class LoaderSpy: FeedLoader {
@@ -76,4 +74,15 @@ final class FeedViewControllerTest: XCTestCase {
         trackMemoryLeak(sut, file: file, line: line)
         return (sut, loader)
     }
+}
+
+private extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        allTargets.forEach({ target in
+            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach({
+                (target as NSObject).perform(Selector($0))
+            })
+        })
+    }
+    
 }

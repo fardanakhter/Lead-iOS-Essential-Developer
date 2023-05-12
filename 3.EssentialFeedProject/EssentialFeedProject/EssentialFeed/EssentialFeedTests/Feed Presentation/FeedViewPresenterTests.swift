42808ab678cb3fa1364ed2ea585822c1efb5df47
Fardan Akhter <fardan_akhter@hotmail.com>
2023-05-13 01:51:34 +0500

    Requests 'display(isLoading:)' event on calling 'didStartLoadingFeed' function

--- 3.EssentialFeedProject/EssentialFeedProject/EssentialFeed/EssentialFeedTests/Feed Presentation/FeedViewPresenterTests.swift
+++ 3.EssentialFeedProject/EssentialFeedProject/EssentialFeed/EssentialFeedTests/Feed Presentation/FeedViewPresenterTests.swift
@@ -7,8 +7,24 @@
 
 import XCTest
 
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
+
+protocol FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel)
+}
+
 final class FeedViewPresenter {
+    let loadingView: FeedLoadingView
     
+    init(loadingView: FeedLoadingView) {
+        self.loadingView = loadingView
+    }
+    
+    func didStartLoadingFeed() {
+        loadingView.display(FeedLoadingViewModel(isLoading: true))
+    }
 }
 
 final class FeedViewPresenterTests: XCTestCase {
@@ -16,13 +32,30 @@ final class FeedViewPresenterTests: XCTestCase {
     func test_init_doesnotRequestEvent() {
         let view = FeedViewSpy()
         
-        let _ = FeedViewPresenter()
+        let _ = FeedViewPresenter(loadingView: view)
         
         XCTAssertEqual(view.messages, [])
     }
     
-    private class FeedViewSpy {
-        private(set) var messages = [String]()
+    func test_didStartLoadingFeed_requestsFeedLoading() {
+        let view = FeedViewSpy()
+        
+        let sut = FeedViewPresenter(loadingView: view)
+        sut.didStartLoadingFeed()
+        
+        XCTAssertEqual(view.messages, [.display(isLoading: true)])
+    }
+    
+    private class FeedViewSpy: FeedLoadingView {
+        private(set) var messages = [Message]()
+        
+        enum Message: Equatable {
+            case display(isLoading: Bool)
+        }
+        
+        func display(_ viewModel: FeedLoadingViewModel) {
+            messages.append(.display(isLoading: viewModel.isLoading))
+        }
     }
     
 }

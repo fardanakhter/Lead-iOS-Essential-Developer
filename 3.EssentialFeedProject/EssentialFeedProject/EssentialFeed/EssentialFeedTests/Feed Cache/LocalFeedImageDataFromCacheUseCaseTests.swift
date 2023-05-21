cc32bd79459a8454151aa12e31be58a5944603c3
Fardan Akhter <fardan_akhter@hotmail.com>
2023-05-21 21:14:49 +0500

    Load command request image data

--- 3.EssentialFeedProject/EssentialFeedProject/EssentialFeed/EssentialFeedTests/Feed Cache/LocalFeedImageDataFromCacheUseCaseTests.swift
+++ 3.EssentialFeedProject/EssentialFeedProject/EssentialFeed/EssentialFeedTests/Feed Cache/LocalFeedImageDataFromCacheUseCaseTests.swift
@@ -7,13 +7,27 @@
 
 import XCTest
 
-public protocol FeedImageDataStore {}
+public protocol FeedImageDataStore {
+    typealias CachedFeedImageData = (data: Data, timestamp: Date)
+    typealias LoadResult = Result<CachedFeedImageData?, Error>
+    typealias LoadCompletion = (LoadResult) -> Void
+
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    func loadFeedImageDataCache(with url: URL, completion: @escaping LoadCompletion)
+}
 
 final class LocalFeedImageDataLoader {
-    let store: FeedImageDataStore
+    private let store: FeedImageDataStore
     
     init(store: FeedImageDataStore) {
         self.store = store
+    }
+    
+    typealias Result = Swift.Result<Data,Error>
+    
+    func load(_ url: URL, completion: @escaping (Result) -> Void) {
+        store.loadFeedImageDataCache(with: url) { _ in }
     }
 }
 
@@ -23,6 +37,15 @@ class LocalFeedImageDataFromCacheUseCaseTests: XCTestCase {
         let (_, store) = makeSUT()
         
         XCTAssertEqual(store.requestedURLs, [])
+    }
+    
+    func test_load_requestsImageData() {
+        let (sut, store) = makeSUT()
+        let imageURL = anyURL()
+        
+        sut.load(imageURL) { _ in }
+        
+        XCTAssertEqual(store.requestedURLs, [imageURL])
     }
     
     private func makeSUT() -> (LocalFeedImageDataLoader, FeedImageDataStoreSpy) {
@@ -35,5 +58,9 @@ class LocalFeedImageDataFromCacheUseCaseTests: XCTestCase {
     
     private class FeedImageDataStoreSpy: FeedImageDataStore {
         private(set) var requestedURLs = [URL]()
+        
+        func loadFeedImageDataCache(with url: URL, completion: @escaping LoadCompletion) {
+            requestedURLs.append(url)
+        }
     }
 }

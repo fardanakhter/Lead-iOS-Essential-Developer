0e9df1348168d1fc275dd0049115f25f182e0f7a
Fardan Akhter <fardan_akhter@hotmail.com>
2023-05-27 07:03:54 +0500

    Remove 'FeedImageDataLoaderTask' type from 'FeedImageDataLoaderCacheDecorator', since task should be handled by decoratee

--- 4.EssentialApp/EssentialApp/EssentialApp/Helper/FeedImageDataLoaderCacheDecorator.swift
+++ 4.EssentialApp/EssentialApp/EssentialApp/Helper/FeedImageDataLoaderCacheDecorator.swift
@@ -11,38 +11,17 @@ public class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
     private let decoratee: FeedImageDataLoader
     private let cache: FeedImageDataCache
     
-    private class TaskWrapper: FeedImageDataLoaderTask {
-        private var callback: ((FeedImageDataLoader.Result) -> Void)?
-        var wrapper: FeedImageDataLoaderTask?
-        
-        init(callback: @escaping ((FeedImageDataLoader.Result) -> Void)) {
-            self.callback = callback
-        }
-        
-        func complete(with result: FeedImageDataLoader.Result) {
-            callback?(result)
-        }
-        
-        func cancel() {
-            wrapper?.cancel()
-            wrapper = nil
-            callback = nil
-        }
-    }
-    
     public init(decoratee: FeedImageDataLoader, cache: FeedImageDataCache) {
         self.decoratee = decoratee
         self.cache = cache
     }
     
     public func load(_ url: URL, completion: @escaping (Result<Data, Error>) -> Void) -> FeedImageDataLoaderTask {
-        let task = TaskWrapper(callback: completion)
-        task.wrapper = decoratee.load(url) {[weak self] result in
-            task.complete(with: result.map {
+        return decoratee.load(url) {[weak self] result in
+            completion(result.map {
                 self?.cache.save($0, with: url){ _ in }
                 return $0
             })
         }
-        return task
     }
 }

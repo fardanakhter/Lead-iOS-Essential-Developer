0ff4c669475634e660c2b14083afc25ada159528
Fardan Akhter <fardan_akhter@hotmail.com>
2023-05-29 19:08:18 +0500

    Add UI tests target 'EssentialAppUIAcceptanceTests' along with its separate scheme

---
+++ 4.EssentialApp/EssentialApp/EssentialAppUIAcceptanceTest/EssentialAppUIAcceptanceTest.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialAppUIAcceptanceTest.swift
+//  EssentialAppUIAcceptanceTest
+//
+//  Created by Fardan Akhter on 29/05/2023.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTest: XCTestCase {
+
+}

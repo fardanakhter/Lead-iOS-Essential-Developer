d503c81b095d71b25e29e5fe4b0bebe190c47fa1
Fardan Akhter <fardan_akhter@hotmail.com>
2023-05-30 07:47:16 +0500

    Add matching view identifier in storyboard for improving the find query

--- 4.EssentialApp/EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ 4.EssentialApp/EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -11,8 +11,13 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
 
     func test_onLaunch_displaysRemoteFeedWhenAppHasConnectivity() {
         let app = XCUIApplication()
+        
         app.launch()
         
-        XCTAssertEqual(app.cells.count, 6)
+        let feedCells = app.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(feedCells.count, 6)
+        
+        let feedImage = app.images.matching(identifier: "feed-image-view").firstMatch
+        XCTAssertTrue(feedImage.exists)
     }
 }

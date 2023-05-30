a5547f780bfab01c9449f99f0d723f60603a2887
Fardan Akhter <fardan_akhter@hotmail.com>
2023-05-30 08:06:13 +0500

    Displays cached feed on launch when app has no connectivity

--- 4.EssentialApp/EssentialApp/EssentialApp/SceneDelegate.swift
+++ 4.EssentialApp/EssentialApp/EssentialApp/SceneDelegate.swift
@@ -20,16 +20,44 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let _ = (scene as? UIWindowScene) else { return }
         
         let url = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5d1c78f21e661a0001ce7cfd/1562147059075/feed-case-study-v1-api-feed.json")!
+        
         let session = URLSession(configuration: .ephemeral)
-        let client = URLSessionHTTPClient(session: session)
+        let client = makeHttpClient(with: session)
         let remoteFeedLoader = RemoteFeedLoader(url: url, httpClient: client)
         let remoteImageLoader = RemoteFeedImageDataLoader(client)
-        let feedViewController = FeedUIComposer.feedUIComposedWith(feedLoader: remoteFeedLoader,
-                                                                   imageLoader: remoteImageLoader)
+        
+        let localStoreURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("feedStore.store")
+        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL, bundle: Bundle(for: CoreDataFeedStore.self))
+        let localFeedLoader = LocalFeedLoader(store: localStore, timestamp: Date.init)
+        let localImageLoader = LocalFeedImageDataLoader(store: localStore)
+        
+        let feedFallbackLoader = FeedLoaderWithFallbackComposite(primary: FeedLoaderCacheDecorator(decoratee: remoteFeedLoader, cache: localFeedLoader), fallback: localFeedLoader)
+        
+        let feedImageFallbackLoader = FeedImageDataLoaderWithFallbackComposite(primary: localImageLoader, fallback: FeedImageDataLoaderCacheDecorator(decoratee: remoteImageLoader, cache: localImageLoader))
+        
+        let feedViewController = FeedUIComposer.feedUIComposedWith(feedLoader: feedFallbackLoader, imageLoader: feedImageFallbackLoader)
         window?.rootViewController = feedViewController
         
     }
+    
+    private func makeHttpClient(with session: URLSession) -> HTTPClient {
+        if let connectivity = UserDefaults.standard.string(forKey: "connectivity"), connectivity == "offline" {
+            return FailingHTTPClient()
+        }
+        return URLSessionHTTPClient(session: session)
+    }
 
+    private struct FailingHTTPClient: HTTPClient {
+        private struct Task: HTTPClientTask {
+            func cancel() {}
+        }
+        
+        func get(_ url: URL, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) -> HTTPClientTask {
+            completion(.failure(NSError(domain: "", code: 0)))
+            return Task()
+        }
+    }
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
--- 4.EssentialApp/EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ 4.EssentialApp/EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -20,4 +20,19 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
         let feedImage = app.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(feedImage.exists)
     }
+    
+    func test_onLaunch_displaysCachedFeedWhenAppHasNoConnectivity() {
+        let onlineApp = XCUIApplication()
+        onlineApp.launch()
+
+        let offlineApp = XCUIApplication()
+        offlineApp.launchArguments = ["-connectivity", "offline"]
+        offlineApp.launch()
+
+        let cachedFeedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(cachedFeedCells.count, 6)
+        
+        let firstCachedImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
+        XCTAssertTrue(firstCachedImage.exists)
+    }
 }

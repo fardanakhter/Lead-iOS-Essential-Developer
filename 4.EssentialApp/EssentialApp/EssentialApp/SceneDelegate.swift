faf86c70d12cb1176b4da3f01b988cdb2a3f8197
Fardan Akhter <fardan_akhter@hotmail.com>
2023-06-01 11:30:56 +0500

    Remove slow 'EssentialFeedUIAcceptanceTests' in favour of fast Integration test

--- 4.EssentialApp/EssentialApp/EssentialApp/SceneDelegate.swift
+++ 4.EssentialApp/EssentialApp/EssentialApp/SceneDelegate.swift
@@ -14,14 +14,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
-    let localStoreURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("feedStore.store")
-    
     private lazy var httpClient: HTTPClient = {
         URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
     }()
     
     private lazy var store: FeedStore & FeedImageDataStore = {
-        try! CoreDataFeedStore(storeURL: localStoreURL, bundle: Bundle(for: CoreDataFeedStore.self))
+        try! CoreDataFeedStore(storeURL: NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("feedStore.store"),
+                               bundle: Bundle(for: CoreDataFeedStore.self))
     }()
     
     convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
@@ -38,10 +37,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func configureWindow() {
         let url = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5d1c78f21e661a0001ce7cfd/1562147059075/feed-case-study-v1-api-feed.json")!
         
-        let client = makeHttpClient()
-        
-        let remoteFeedLoader = RemoteFeedLoader(url: url, httpClient: client)
-        let remoteImageLoader = RemoteFeedImageDataLoader(client)
+        let remoteFeedLoader = RemoteFeedLoader(url: url, httpClient: httpClient)
+        let remoteImageLoader = RemoteFeedImageDataLoader(httpClient)
         
         let localFeedLoader = LocalFeedLoader(store: store, timestamp: Date.init)
         let localImageLoader = LocalFeedImageDataLoader(store: store)
@@ -53,39 +50,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window?.rootViewController = navigationController
     }
-    
-    func makeHttpClient() -> HTTPClient {
-        httpClient
-    }
-    
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
 

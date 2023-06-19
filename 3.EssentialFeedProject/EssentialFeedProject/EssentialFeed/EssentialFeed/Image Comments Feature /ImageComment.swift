3e0f2b6cc1316aee54bd26e37507b7c59bec6856
Fardan Akhter <fardan_akhter@hotmail.com>
2023-06-19 09:33:31 +0500

    Add 'ImageComment' data model and 'ImageCommentLoader' class

---
+++ 3.EssentialFeedProject/EssentialFeedProject/EssentialFeed/EssentialFeed/Image Comments Feature /ImageComment.swift
@@ -0,0 +1,23 @@
+//
+//  ImageComment.swift
+//  EssentialFeed
+//
+//  Created by Fardan Akhter on 19/06/2023.
+//
+
+import Foundation
+
+public struct ImageComment: Hashable {
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+    
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
+

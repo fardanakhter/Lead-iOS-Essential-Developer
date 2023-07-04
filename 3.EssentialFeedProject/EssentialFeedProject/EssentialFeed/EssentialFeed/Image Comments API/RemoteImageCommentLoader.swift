a34baeaaafb7b4f6cdd27d7e645df18fd3fd89ac
Fardan Akhter <fardan_akhter@hotmail.com>
2023-07-05 00:10:28 +0500

    Replace 'RemoteImageCommentLoader' with 'RemoteLoader' to remove duplication

--- 3.EssentialFeedProject/EssentialFeedProject/EssentialFeed/EssentialFeed/Image Comments API/RemoteImageCommentLoader.swift
+++ 3.EssentialFeedProject/EssentialFeedProject/EssentialFeed/EssentialFeed/Image Comments API/RemoteImageCommentLoader.swift
@@ -7,43 +7,10 @@
 
 import Foundation
 
-public final class RemoteImageCommentLoader: ImageCommentLoader {
-    private let url: URL
-    private let httpClient: HTTPClient
-    
-    public enum Error: Swift.Error {
-        case noConnection
-        case invalidData
-    }
-    
-    public typealias Result = ImageCommentLoader.Result
-    
-    public init(url: URL, httpClient: HTTPClient) {
-        self.url = url
-        self.httpClient = httpClient
-    }
-    
-    public func load(completion: @escaping (Result) -> Void) {
-        let _ = self.httpClient.get(url) { [weak self] result in
-            guard let _ = self else { return }
-            
-            switch result {
-            case let .success((data, response)):
-                completion(RemoteImageCommentLoader.map(data: data, response: response))
-            case .failure:
-                completion(.failure(Error.noConnection))
-            }
-        }
-    }
-    
-    private static func map(data: Data, response: HTTPURLResponse) -> Result {
-        do {
-            let items = try ImageCommentMapper.map(from: data, and: response)
-            return .success(items)
-        }
-        catch(let error) {
-            return .failure(error)
-        }
+public typealias RemoteImageCommentLoader = RemoteLoader<[ImageComment]>
+
+extension RemoteImageCommentLoader {
+    public convenience init(url: URL, httpClient: HTTPClient) {
+        self.init(url: url, httpClient: httpClient, mapper: ImageCommentMapper.map)
     }
 }
-
--- 3.EssentialFeedProject/EssentialFeedProject/EssentialFeed/EssentialFeedTests/Image Comments API/LoadImageCommentRemoteUseCaseTests.swift
+++ 3.EssentialFeedProject/EssentialFeedProject/EssentialFeed/EssentialFeedTests/Image Comments API/LoadImageCommentRemoteUseCaseTests.swift
@@ -11,40 +11,6 @@ import EssentialFeed
 
 class LoadImageCommentRemoteUseCaseTests: XCTestCase {
     
-    func test_init_doesNotRequestDataFromURL() {
-        let (_, client) = makeSUT()
-        
-        XCTAssertTrue(client.requestedURLs.isEmpty)
-    }
-    
-    func test_load_requestsDataFromURL() {
-        let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT(url)
-        
-        sut.load() { _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url])
-    }
-    
-    func test_loadTwice_requestsDataFromURLTwice() {
-        let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT(url)
-        
-        sut.load() { _ in }
-        sut.load() { _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url, url])
-    }
-    
-    func test_load_deliversErrorOnClientError() {
-        let (sut, client) = makeSUT()
-        
-        expect(sut, toCompleteWith: failure(.noConnection), when: {
-            let clientError = NSError(domain: "Test Error", code: 0)
-            client.complete(withError: clientError)
-        })
-    }
-    
     func test_load_deliversErrorOnNon2xxResponse() {
         let (sut, client) = makeSUT()
         
@@ -105,24 +71,6 @@ class LoadImageCommentRemoteUseCaseTests: XCTestCase {
                 client.complete(withStatusCode: code, data: itemsData, at: index)
             })
         }
-    }
-    
-    func test_load_doesNotInvokeCompletionWithResultAfterSutDeallocated() {
-        let url = URL(string: "http://any-url.com")!
-        let client = HTTPClientSpy()
-        var sut: RemoteImageCommentLoader? = RemoteImageCommentLoader(url: url, httpClient: client)
-        
-        var capturedResults = [RemoteImageCommentLoader.Result]()
-        sut?.load() { result in
-            capturedResults.append(result)
-        }
-        
-        sut = nil
-        [150, 190, 200, 250, 299].forEach {
-            client.complete(withStatusCode: $0, data: makeItemJSON([:]))
-        }
-        
-        XCTAssertTrue(capturedResults.isEmpty)
     }
     
     // MARK: - Helpers

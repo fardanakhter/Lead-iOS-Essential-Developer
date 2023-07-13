55a54911f9f0e52f7b148ddf579db6905289d4b2
Fardan Akhter <fardan_akhter@hotmail.com>
2023-07-13 17:10:17 +0500

    Move ResourceLoadingView and ViewModel to Shared Presentation module

--- 3.EssentialFeedProject/EssentialFeedProject/EssentialFeed/EssentialFeed/Shared  Presentation/LoadResourcePresenter.swift
+++ 3.EssentialFeedProject/EssentialFeedProject/EssentialFeed/EssentialFeed/Shared  Presentation/LoadResourcePresenter.swift
@@ -7,22 +7,6 @@
 
 import Foundation
 
-// Load Resource
-public protocol ResourceView {
-    associatedtype ResourceViewModel
-    func display(_ viewModel: ResourceViewModel)
-}
-
-// Loading
-public protocol ResourceLoadingView {
-    func display(_ viewModel: ResourceLoadingViewModel)
-}
-
-public struct ResourceLoadingViewModel {
-    public let isLoading: Bool
-}
-
-// Generic Presenter
 public final class LoadResourcePresenter<Resource, View: ResourceView>  {
     public typealias Mapper = (Resource) -> View.ResourceViewModel
     
---
+++ 3.EssentialFeedProject/EssentialFeedProject/EssentialFeed/EssentialFeed/Shared  Presentation/ResourceLoadingView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceLoadingView.swift
+//  EssentialFeed
+//
+//  Created by Fardan Akhter on 13/07/2023.
+//
+
+import Foundation
+
+public protocol ResourceLoadingView {
+    func display(_ viewModel: ResourceLoadingViewModel)
+}

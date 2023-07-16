4577c89e5218b01f2a988a62dc7d22a4de610bc7
Fardan Akhter <fardan_akhter@hotmail.com>
2023-07-16 16:46:26 +0500

    Remove unused model classes

--- 3.EssentialFeedProject/EssentialFeedProject/EssentialFeed/EssentialFeed/Feed Presentation/FeedViewPresenter.swift
+++ 3.EssentialFeedProject/EssentialFeedProject/EssentialFeed/EssentialFeed/Feed Presentation/FeedViewPresenter.swift
@@ -7,37 +7,12 @@
 
 import Foundation
 
-public protocol FeedView {
-    func display(_ viewModel: FeedViewModel)
-}
-
 public final class FeedViewPresenter {
-    private let feedView: FeedView
-    private let loadingView: ResourceLoadingView
-    
-    public init(feedView: FeedView, loadingView: ResourceLoadingView) {
-        self.feedView = feedView
-        self.loadingView = loadingView
-    }
-    
    public static var feedViewTitle: String {
         NSLocalizedString("FEED_VIEW_TITLE",
                           tableName: "Feed",
                           bundle: Bundle(for: FeedViewPresenter.self),
                           comment: "Title for Feed View")
-    }
-    
-    public func didStartLoadingFeed() {
-        loadingView.display(ResourceLoadingViewModel(isLoading: true))
-    }
-    
-    public func didCompleteLoadingFeed(with feed: [FeedImage]) {
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
-        feedView.display(Self.map(feed))
-    }
-    
-    public func didCompleteLoadingFeed(with error: Error) {
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
     }
     
     static public func map(_ feed: [FeedImage]) -> FeedViewModel {

9a7deaa4b999e7fe9594f38af8f82eca9f7f11c5
Fardan Akhter <fardan_akhter@hotmail.com>
2023-07-31 23:39:59 +0500

    Refactor comments loader adaptor to use Combine framework

--- 4.EssentialApp/EssentialApp/EssentialApp/CommentsLoaderPresentationAdaptor.swift
+++ 4.EssentialApp/EssentialApp/EssentialApp/CommentsLoaderPresentationAdaptor.swift
@@ -5,27 +5,45 @@
 //  Created by Fardan Akhter on 31/07/2023.
 //
 
-import Foundation
+import Combine
 import EssentialFeed
 import EssentialFeediOS
 
 final class CommentsLoaderPresentationAdaptor: ListRefreshViewControllerDelegate {
-    private let loader: ImageCommentLoader
+    
+    private struct ImageCommentLoaderDecorator {
+        private let decoratee: ImageCommentLoader
+        
+        init(decoratee: ImageCommentLoader) {
+            self.decoratee = decoratee
+        }
+        
+        func loadPublisher() -> AnyPublisher<[ImageComment], Swift.Error> {
+            Future(decoratee.load).eraseToAnyPublisher()
+        }
+    }
+    
+    private var cancellable: Set<AnyCancellable> = .init()
+    private var loader: ImageCommentLoaderDecorator
+    
     var presenter: LoadResourcePresenter<[ImageComment], CommentsViewAdapter>?
     
     init(loader: ImageCommentLoader) {
-        self.loader = loader
+        self.loader = ImageCommentLoaderDecorator(decoratee: loader)
     }
     
     func didStartLoadingList() {
         presenter?.didStartLoadingResource()
-        loader.load { [weak self] result in
-            switch result {
-            case .success(let feed):
-                self?.presenter?.didCompleteLoading(with: feed)
-            case .failure(let error):
-                self?.presenter?.didCompleteLoadingResource(with: error)
+        loader.loadPublisher()
+            .sink { [weak self] completion in
+                guard let self else { return }
+                if case let .failure(error) = completion {
+                    self.presenter?.didCompleteLoadingResource(with: error)
+                }
+            } receiveValue: { [weak self] feed in
+                guard let self else { return }
+                self.presenter?.didCompleteLoading(with: feed)
             }
-        }
+            .store(in: &cancellable)
     }
 }
